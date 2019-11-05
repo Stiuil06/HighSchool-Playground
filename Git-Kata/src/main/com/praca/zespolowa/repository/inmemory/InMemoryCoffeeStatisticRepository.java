826c826411ef3b366dccc274cc68a3ea5af5e9b7
John Doe <JohnDoeSuperMail@supermail.ru>
2019-11-05 13:44:07 +0100

    Impement database coffee

--- Git-Kata/src/main/com/praca/zespolowa/repository/inmemory/InMemoryCoffeeStatisticRepository.java
+++ Git-Kata/src/main/com/praca/zespolowa/repository/inmemory/InMemoryCoffeeStatisticRepository.java
@@ -1,36 +1,48 @@
 package com.praca.zespolowa.repository.inmemory;
 
 
+import com.praca.zespolowa.config.Config;
 import com.praca.zespolowa.exception.DataCreationException;
+import com.praca.zespolowa.model.Coffee;
 import com.praca.zespolowa.repository.CoffeeStatisticRepository;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class InMemoryCoffeeStatisticRepository implements CoffeeStatisticRepository {
 
-    //TODO ZADANIE 2 stwórz sposób przechowywania statystyk (nazwaKawy -> iloscZrobionychKaw)
+    private Map<String, Integer> coffees;
+
+    public InMemoryCoffeeStatisticRepository() {
+        coffees = new HashMap<>();
+        for (Config.COFFE value : Config.COFFE.values()) {
+            coffees.put(value.toString(), 0);
+        }
+    }
 
     @Override
     public boolean increamentCoffe(String coffeName) throws DataCreationException {
-        //TODO ZADANIE 2 napisz implementację metody która będzie inkrementowała ilość zrobionych kaw w naszej bazie
-        return false;
+        int x = coffees.get(coffeName);
+        coffees.replace(coffeName, x+1);
+        return true;
     }
 
     @Override
     public Map<String,Integer> findAll() {
-        //TODO ZADANIE 2 napisz implementację metody która będzie zwracała statystyki zrobionych kaw
-        return null;
+        return coffees;
     }
 
     @Override
     public boolean resetAllStatistics() {
-        //TODO ZADANIE 2 napisz implementację metody zresetuje wszystkie statystyki zrobionych kaw
-        return false;
+        coffees = new HashMap<>();
+        return true;
     }
 
     @Override
     public boolean resetStatisticFor(String coffeName) {
-        //TODO ZADANIE 2 napisz implementację metody zresetuje  statystyki wybranej kawy
-        return false;
+        coffees.replace(coffeName, 0);
+        return true;
     }
 }
